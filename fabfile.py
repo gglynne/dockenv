8652264133dee318e3d1f256566c577731d3b7d3
gglynne@DELL <gglynne@yahoo.com>
2015-03-05 16:02:53 +0000

    Thu Mar  5 16:02:53 GMT 2015

--- fabfile.py
+++ fabfile.py
@@ -3,43 +3,59 @@
 # vim: foldmethod=marker:foldmarker={{{,}}}:cms=#%s
 
 """
-2015-03-05 13:37
+fab script to set up a docker container on server machine
 
-    [[differences  between python 2.7 and 3.x.wiki]]
-    
-    A curated list of awesome Python frameworks, libraries and software:
-        https://github.com/vinta/awesome-python
-            
-    print string formats:
-        http://www.pererikstrandberg.se/blog/index.cgi?page=PythonPrintStringFormatCheatSheet
-            
-    Useful modules:
-        https://wiki.python.org/moin/UsefulModules
-
-    django packages compared
-        https://www.djangopackages.com/grids/g/oauth/
+http://docs.fabfile.org/en/latest/api/core/operations.html
+http://docs.fabfile.org/en/1.5/api/core/context_managers.html#fabric.context_managers.shell_env
+https://fabric.readthedocs.org/en/1.8/usage/env.html#gateway
+https://www.digitalocean.com/community/tutorials/how-to-use-fabric-to-automate-administration-tasks-and-deployments
 
 """
 
 from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
-
+from fabric.context_managers import settings, shell_env
 from fabric.api import local
+from fabric.operations import run as runfabric
 
+# gateway is a windows box with boot2docker installed: it has localhost ssh access to boot2docker
 env.gateway = 'backup_user@server: 65371'
+
+# boot2docker running on gateway
 env.hosts = ['docker@192.168.59.103']
 env.password= 'tcuser'
 env.shell= '/bin/sh -l -c'
 code_dir = '~/dockenv'
 repo_url = "https://github.com/gglynne/dockenv.git"
 
+dry=True
+dry=False
+
+def run(*args,**kwargs):
+    if dry:
+        print args, kwargs
+    else:
+        runfabric(*args, **kwargs)
 
 def clone():
+    """
+    re-clone repo in home
+    """
+
+    run("""
+        if [[ -d %s ]]; then 
+            rm -rf %s; 
+        fi
+    """ % ( code_dir,code_dir ))
+
     run("git clone " + repo_url )
 
 
 def push():
+    """
+    push changes from localdir onto github, then pull them onto the server
+    """
     local('git add .')
     local('git commit -a -m "`date`"');
     local('git push')
@@ -48,3 +64,20 @@ def push():
 
         #run('ls')
     #print(env)
+
+#global WINHOME
+#with settings(host_string=GATEWAY):
+    #WINHOME=run('cygpath ${USERPROFILE}')
+
+#def test():
+    #with settings(host_string=GATEWAY):
+        #with shell_env(HOME = WINHOME, BOOT2DOCKER = BOOT2DOCKER):
+            #run('"$BOOT2DOCKER" ip')
+
+        #with shell_env(NAME='tester'):
+            #run('echo $NAME')
+
+    #@$(eval IP := $(shell export HOME=${WINHOME};${BOOT2DOCKER} ip 2> /dev/null) )
+
+
+
