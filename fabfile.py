6e699bfdd1f3a3f1b1eed6470d93a99c2c8f7cb0
gglynne@DELL <gglynne@yahoo.com>
2015-03-11 10:11:05 +0000

    Wed Mar 11 10:11:05 GMT 2015

--- fabfile.py
+++ fabfile.py
@@ -31,17 +31,18 @@ from fabric.operations import run as runfabric
 
 
 # gateway is a windows box with boot2docker installed: it has localhost ssh access to boot2docker
-env.gateway = 'backup_user@server: 65371'
+env.gateway = 'backup_user@server:65371'
 
 # boot2docker running on gateway
-env.hosts = ['docker@192.168.59.104']
+env.hosts = ['docker@192.168.59.103']
 env.password= 'tcuser'
 env.shell= '/bin/sh -l -c'
 code_dir = '~/dockenv'
 repo_url = "https://github.com/gglynne/dockenv.git"
 
-# http proxy on devmachine port 8888 for caching docker images
-http_proxy='192.168.1.106:8888'
+# http proxy on devmachine port 
+http_proxy='http://192.168.1.109:8123'
+
 cachedir='~/.proxycache'
 
 #dry=True
@@ -53,21 +54,44 @@ def run(*args,**kwargs):
     else:
         runfabric(*args, **kwargs)
 
+def restart():
+    sudo('/etc/init.d/docker restart')
+
+def copykeys():
+    with settings(host_string=env.gateway):
+        run("ssh-copy-id %s" % env.hosts[0])
+
+def cache():
+    #local("if [[ ! -d %s ]]; then mkdir %s; fi" % (cachedir, cachedir) )
+    #local("../http-replicator/http-replicator -r %s -p 8888 --daemon /tmp/replicator.log" % cachedir)
+    sudo('echo export HTTP_PROXY=%s > /var/lib/boot2docker/profile' % http_proxy);
+    sudo('echo export http_proxy=%s >> /var/lib/boot2docker/profile' % http_proxy)
+    sudo('echo export HTTPS_PROXY=%s >> /var/lib/boot2docker/profile' % http_proxy)
+    sudo('echo export https_proxy=%s >> /var/lib/boot2docker/profile' % http_proxy)
+    sudo('echo export NO_PROXY=localhost,127.0.0.0/8,::1,/var/run/docker.sock >> /var/lib/boot2docker/profile')
+    sudo('echo export no_proxy=localhost,127.0.0.0/8,::1,/var/run/docker.sock  >> /var/lib/boot2docker/profile')
+    sudo('/etc/init.d/docker restart')
+
+def testcache():
+    run(". /var/lib/boot2docker/profile")
+    run("env | grep -i http")
+    run("curl http://google.com")
+
+def pullimages():
+    run("docker pull busybox")
+    run("docker pull python:3.4.2")
+    run("docker pull postgres:9.3")
+
 def fig(cmd):
     return run(""" docker run --rm -it -v %s:/app -v /var/run/docker.sock:/var/run/docker.sock \
         -e FIG_PROJECT_NAME=%s dduportal/fig \
         %s \
     """ % ( code_dir,code_dir, cmd ) ) 
 
-def cache():
-    #local("if [[ ! -d %s ]]; then mkdir %s; fi" % (cachedir, cachedir) )
-    #local("../http-replicator/http-replicator -r %s -p 8888 --daemon /tmp/replicator.log" % cachedir)
-    sudo('echo  export HTTP_PROXY=192.168.1.106:8888 > /var/lib/boot2docker/profile');
-    sudo('echo  export HTTPS_PROXY=192.168.1.106:8888 >> /var/lib/boot2docker/profile');
-    sudo('/etc/init.d/docker restart')
 
 
-def init():
+
+def up():
     fig("up")
 
 def clone():
